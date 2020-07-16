809d30d969fa45e114bd245beaba1602f7f8f8fa
gabriel <akingdebased@gmail.com>
2020-07-16 13:01:49 -0700

    implement navbar hover menu

--- js/main.js
+++ js/main.js
@@ -11,6 +11,23 @@ window.addEventListener('scroll', () => {
 
 const announcementBannerEl = document.querySelector('.fls__announcement-banner');
 
-document.querySelector('.fls__announcement-banner-close').addEventListener('click', () => {
-	announcementBannerEl.classList.add('animate__slideOutDown');
+if (announcementBannerEl) {
+	document.querySelector('.fls__announcement-banner-close').addEventListener('click', () => {
+		announcementBannerEl.classList.add('animate__slideOutDown');
+	});
+}
+
+const dropdownEl = document.querySelector('.fls__navbar-dropdown');
+
+document.querySelectorAll('.navbar--fls .tabs li').forEach(navbarItemEl => {
+	navbarItemEl.addEventListener('mouseenter', e => {
+		const leftPos = e.target.getBoundingClientRect().x,
+			topOffset = -10,
+			topPos = e.target.getBoundingClientRect().height + topOffset;
+
+		dropdownEl.setAttribute('style', `top:${topPos}px;left:${leftPos}px`);
+		dropdownEl.classList.add('fls--opaque');
+	})
+
+	navbarItemEl.addEventListener('mouseleave', () => dropdownEl.classList.remove('fls--opaque'));
 });
