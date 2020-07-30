38b66826b308707cb08ed07a0f58e80bac8483f3
gabriel <akingdebased@gmail.com>
2020-07-30 12:44:29 -0700

    add markdown plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,5 +19,13 @@ module.exports = {
 				display: 'swap',
 			},
 		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `markdown-pages`,
+				path: `${__dirname}/blog`,
+			},
+		},
+		`gatsby-transformer-remark`,
 	],
 };
