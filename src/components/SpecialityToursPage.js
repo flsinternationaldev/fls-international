adacef3d23ab298822384b4ae6be142dddeb1776
gabriel <akingdebased@gmail.com>
2020-09-04 04:44:33 -0700

    implement speciality tour page design

--- src/components/SpecialityToursPage.js
+++ src/components/SpecialityToursPage.js
@@ -32,21 +32,30 @@ export const SpecialityTourPageTemplate = ({
 				</div>
 
 				<div className="column is-9-desktop is-full-tablet">
-					<div className="column is-full">
-						<div className="fls__location-post-carousel">
-							<Slick {...slickSettings}>
-								{specialityTourData.carousel_images.map(
-									carouselImagePath => {
-										return (
-											<img
-												key={carouselImagePath}
-												src={carouselImagePath}
-												alt={`${specialityTourData.name} carousel image`}
-											/>
-										);
-									}
-								)}
-							</Slick>
+					<div className="columns is-multiline">
+						<div className="column is-full">
+							{/* TODO: Again, these 'programs' classes need genericizing */}
+							<h2 className="programs__post-title">
+								{specialityTourData.name}
+							</h2>
+						</div>
+
+						<div className="column is-full">
+							<div className="fls__location-post-carousel">
+								<Slick {...slickSettings}>
+									{specialityTourData.carousel_images.map(
+										carouselImagePath => {
+											return (
+												<img
+													key={carouselImagePath}
+													src={carouselImagePath}
+													alt={`${specialityTourData.name} carousel image`}
+												/>
+											);
+										}
+									)}
+								</Slick>
+							</div>
 						</div>
 					</div>
 
@@ -59,6 +68,7 @@ export const SpecialityTourPageTemplate = ({
 								/>{' '}
 								{`Age ${specialityTourData.speciality_tour_details.minimum_age}+`}
 							</span>
+
 							<span className="fls-post__subhero-item">
 								<FontAwesomeIcon
 									className="fls-post__subhero-icon"
@@ -66,6 +76,130 @@ export const SpecialityTourPageTemplate = ({
 								/>{' '}
 								{`${specialityTourData.speciality_tour_details.number_of_weeks} weeks`}
 							</span>
+						</div>
+					</div>
+
+					<div className="column is-full">
+						{specialityTourData.speciality_tour_description}
+					</div>
+
+					<div className="column is-full">
+						<div className="fls-post__copy-container fls-post__copy-container--alt">
+							<h3 className="fls-post__subtitle">
+								Activities and Excursions
+							</h3>
+
+							<MarkdownContent
+								content={
+									specialityTourData.activities_and_excursions
+								}
+								classMap={{
+									h2: 'fls-post__subtitle',
+									p: 'fls-post__paragraph',
+									ul: 'fls__location-post-list',
+									li: 'fls__list-item',
+								}}
+							/>
+						</div>
+					</div>
+
+					<div className="column is-full">
+						<div className="fls-post__copy-container">
+							<h3 className="fls-post__subtitle">Features</h3>
+
+							<MarkdownContent
+								content={specialityTourData.features}
+								classMap={{
+									h2: 'fls-post__subtitle',
+									p: 'fls-post__paragraph',
+									ul: 'fls__location-post-list',
+									li: 'fls__list-item',
+								}}
+							/>
+						</div>
+					</div>
+
+					<div className="column is-full">
+						<div className="fls-post__copy-container fls-post__copy-container--alt">
+							<h3 className="fls-post__subtitle">
+								Accommodatons
+							</h3>
+
+							<MarkdownContent
+								content={specialityTourData.accommodations}
+								classMap={{
+									h2: 'fls-post__subtitle',
+									p: 'fls-post__paragraph',
+									ul: 'fls__location-post-list',
+									li: 'fls__list-item',
+								}}
+							/>
+						</div>
+					</div>
+
+					<div className="column is-full">
+						<div className="fls-post__copy-container">
+							<div className="fls-post__copy-container-header">
+								<h3 className="fls-post__subtitle">
+									Program Dates
+								</h3>
+
+								<span className="fls-post__sample-calendar fls--red">
+									<FontAwesomeIcon
+										className="fls-post__subhero-icon fls--red"
+										icon={faCalendarAlt}
+									/>
+									<a
+										href={
+											specialityTourData.sample_calendar
+										}
+										className="fls--red"
+										target="_blank"
+									>
+										<strong>VIEW A SAMPLE CALENDAR</strong>
+									</a>
+								</span>
+							</div>
+							{/* Don't let the class name fool you, I refuse to
+							use actual tables */}
+
+							{/* TODO: This totally breaks on mobile */}
+							<div className="fls-post__table-header">
+								<div className="columns">
+									<span className="column is-one-quarter">
+										Arrive
+									</span>
+									<span className="column is-one-quarter">
+										Depart
+									</span>
+									<span className="column is-one-quarter">
+										Price
+									</span>
+									<span className="column is-one-quarter"></span>
+								</div>
+							</div>
+							{specialityTourData.program_dates.map(
+								programDate => (
+									<div className="fls-post__table-row">
+										<div className="columns">
+											<span className="column is-one-quarter">
+												{programDate.arrive}
+											</span>
+											<span className="column is-one-quarter">
+												{programDate.depart}
+											</span>
+											<span className="column is-one-quarter">
+												{programDate.price}
+											</span>
+											<span className="column is-one-quarter">
+												<button className="fls__button fls__button--small">
+													Apply
+												</button>
+											</span>
+										</div>
+									</div>
+								)
+							)}
 						</div>
 					</div>
 				</div>
